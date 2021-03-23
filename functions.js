f88866f23bda89dc33caa92e45a5aba7113427b3
DaleCooper59 <duvinagechristopher1@gmail.com>
2021-03-23 12:34:36 +0100

    first commit

---
+++ functions.js
@@ -0,0 +1,12 @@
+//inverser string dans phrase si >= 4 lettres
+
+function spinWords(words){
+    return words.split(' ').map(word => (word.length >= 4) ? reverseStr(word):word).join(' ');
+  }
+  
+  function reverseStr(str){
+    return str.split('').reverse().join('');
+  }
+  
+
+  
