f0181e16deb1c73e7ac8e659dbc6b922aff2273f
DaleCooper59 <duvinagechristopher1@gmail.com>
2021-03-25 15:03:30 +0100

    up

--- functions.js
+++ functions.js
@@ -1,5 +1,4 @@
 //inverser string dans phrase si >= 4 lettres
-
 function spinWords(words){
     return words.split(' ').map(word => (word.length >= 4) ? reverseStr(word):word).join(' ');
   }
@@ -7,10 +6,13 @@ function spinWords(words){
   function reverseStr(str){
     return str.split('').reverse().join('');
   }
+
+
   
 //Trier nombres +ptit au +grand
-
 sort((a, b) => a - b);
+
+
 
 //rechercher si toutes les lettres de L'alphabet dans une phrase
 function isPangram(string){
@@ -20,9 +22,28 @@ function isPangram(string){
   });
 }
 
+
+
+//alterner maj/min
+String.prototype.toAlternatingCase = function () {
+  return this.split('').map(letter => change(letter)).join('');
+}
+
+function change(x){
+   return x.toUpperCase() === x ? x.toLowerCase() : x.toUpperCase();
+}
+        //ou 
+        String.prototype.toAlternatingCase = function () {
+         return this.replace(/[A-Za-z]/g, x => x > "Z" ? x.toUpperCase() : x.toLowerCase()) 
+        }
+
+
+
 //check si un array contient l'élément
 //ex: isInside([55,88,'vache',null],'vache') => true
 function isInside(a,b){
   return a.includes(b);
 }
+
+
 
